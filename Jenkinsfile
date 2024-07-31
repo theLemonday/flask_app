fefc65d135b9c0c38b0cb74b20b6db2ea2d7a8f6
Lemonday <nhathao090703@gmail.com>
2024-07-31 20:58:04 +0700

    add Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline {
+    agent {
+        docker {
+            image: python:3.9-slim
+        }
+    }
+
+    stages {
+        stage('build') {
+            steps {
+                sh 'pip install -r requirements.txt'
+            }
+        }
+        stage('test') {
+            steps {
+                sh 'pytest tests/tests_app.py'
+            }
+        }
+
+    }
+}
--- tests/tests_app.py
+++ tests/tests_app.py
@@ -1,28 +1,37 @@
 import pytest
+
 from app import app
+
+# import app as app
+
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home(client):
-    rv = client.get('/')
-    assert b'Welcome to the Flask App!' in rv.data
+    rv = client.get("/")
+    assert b"Welcome to the Flask App!" in rv.data
+
 
 def test_about(client):
-    rv = client.get('/about')
-    assert b'This is the about page.' in rv.data
+    rv = client.get("/about")
+    assert b"This is the about page." in rv.data
+
 
 def test_api_data(client):
-    rv = client.get('/api/data')
+    rv = client.get("/api/data")
     json_data = rv.get_json()
-    assert json_data['name'] == 'Flask App'
+    assert json_data["name"] == "Flask App"
+
 
 def test_form_get(client):
-    rv = client.get('/form')
+    rv = client.get("/form")
     assert b'<form method="POST">' in rv.data
 
+
 def test_form_post(client):
-    rv = client.post('/form', data={'name': 'Test User'})
-    assert b'Thank you, Test User!' in rv.data
+    rv = client.post("/form", data={"name": "Test User"})
+    assert b"Thank you, Test User!" in rv.data
